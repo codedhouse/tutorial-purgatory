86a84121f1e4701d81ace4c0609488ab5b416f9e
Vivien <vivien-public@pm.me>
2025-12-12 18:45:58 -0500

    Review TypeScript basics

---
+++ typescript/types/primitiveTypes.ts
@@ -0,0 +1,18 @@
+// Declared types
+let isLoggedIn: boolean;
+
+let username: string;
+let userRatings: num;
+username = 'clefairy84';
+userRatings = 2374;
+isLoggedIn = true;
+
+// Inferred types
+let author = 'Frances Hardinge';
+let averageRating = 4.8;
+let isVerified = false;
+
+// number can be an integer or floating point value
+let priceInDollars: number;
+priceInDollars = 45;
+priceInDollars = 7.99;
